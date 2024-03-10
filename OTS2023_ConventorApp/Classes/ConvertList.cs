6372a60a27dd256ed7a91e925cebc80a81ffe4e2
NeonIT56 <jevic.it56.2022@uns.ac.rs>
2024-03-10 12:24:12 +0100

    Length, Mass Converter and Folder Structure Update

---
+++ OTS2023_ConventorApp/Classes/ConvertList.cs
@@ -0,0 +1,23 @@
+﻿using OTS2023_ConventorApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2023_ConventorApp.Classes
+{
+    public class ConvertList
+    {
+        public static double ConvertFromList(string list)
+        {
+            string[] values = list.Split(',');
+            double number = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                number += Double.Parse(values[i]);
+            }
+            return Convert.ToDouble(number);
+        }
+    }
+}
---
+++ OTS2023_ConventorApp/Classes/LengthConverter.cs
@@ -0,0 +1,17 @@
+﻿using OTS2023_ConventorApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2023_ConventorApp.Classes
+{
+    public class LengthConverter : IConvert
+    {
+        public string Convert(string value)
+        {
+            return Math.Round(Double.Parse(value) / 3.281, 3).ToString();   
+        }
+    }
+}
